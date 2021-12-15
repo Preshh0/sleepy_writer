8bd079900911812319bc7caa3abfa57c4a751d8d
Preshh0 <uniquep201@gmail.com>
2021-12-15 03:10:40 +0100

    Created another app 'blog' and created models on it, but it's giving me errors when i try to create posts on the admin page. I'd fix it this night.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-
 # Create your views here.
-class HomePageView(TemplateView):
-    template_name = 'index.html'
 
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
