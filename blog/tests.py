323297aa9edc9ecefb0b108b06d193d388e12dcb
Preshh0 <uniquep201@gmail.com>
2021-12-15 14:22:18 +0100

    Wrote tests for the Post model.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(post_title='just a test')
+
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name= f'{post.post_title}'
+        self.assertEqual(expected_object_name, 'just a test')
+
--- home/tests.py
+++ home/tests.py
@@ -10,3 +10,4 @@ class SimpleTests(TestCase):
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+
