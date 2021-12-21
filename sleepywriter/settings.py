75c0e98605cbe3847b931066fccfee9aa261c287
Preshh0 <uniquep201@gmail.com>
2021-12-21 01:56:16 +0100

    Added delete post, login and logout functionality and then signup feature. The signup feature has a bug tho

--- sleepywriter/settings.py
+++ sleepywriter/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'home',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog'
+LOGOUT_REDIRECT_URL = 'blog'
--- sleepywriter/urls.py
+++ sleepywriter/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
